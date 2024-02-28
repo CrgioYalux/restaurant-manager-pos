e7c442fceec2d294bf8d8724ba35edd101a43a7d
Sergio Yalux <randaledomman1@gmail.com>
2024-02-28 02:09:08 -0300

    fix: do not use props to build class names dynamically

--- frontend/src/input_components/PasswordInput/index.jsx
+++ frontend/src/input_components/PasswordInput/index.jsx
@@ -3,7 +3,43 @@ import { useState, useId } from 'react'
 import OpenEye from '../Icons/OpenEye'
 import ClosedEye from '../Icons/ClosedEye'
 
-const PasswordInput = ({ className = '', label = 'Password', color = 'blue', ...inputProps }) => {
+const colorsByVariant = {
+	initial: {
+		span: 'text-black dark:text-white',
+		input: 'bg-indigo-800 dark:bg-indigo-200 text-indigo-200 dark:text-indigo-800',
+		button: 'text-indigo-800 dark:text-indigo-200',
+	},
+	success: {
+		span: 'text-black dark:text-white',
+		input: 'bg-green-800 dark:bg-green-200 text-green-200 dark:text-green-800',
+		button: 'text-green-800 dark:text-green-200',
+	},
+	error: {
+		span: 'text-black dark:text-white ',
+		input: 'bg-red-800 dark:bg-red-200 text-red-200 dark:text-red-800',
+		button: 'text-red-800 dark:text-red-200',
+	},
+	warning: {
+		span: 'text-black dark:text-white',
+		input: 'bg-yellow-800 dark:bg-yellow-200 text-yellow-200 dark:text-yellow-800',
+		button: 'text-yellow-800 dark:text-yellow-200',
+	},
+}
+
+const defaultClassNames = (variant) => ({
+	label: 'flex items-center gap-2 w-96',
+	span: `flex-none grow-0 min-w-max ${colorsByVariant[variant]?.span}`,
+	div: 'grow shrink basis-full flex gap-1 ml-auto',
+	input: `flex-none w-[24ch] rounded p-1 ${colorsByVariant[variant]?.input}`,
+	button: `grow-0 shrink-0 basis-8 grid place-items-center fill-current ${colorsByVariant[variant]?.button}`,
+})
+
+const PasswordInput = ({
+	classNames = {},
+	label = 'Password',
+	variant = 'initial',
+	...inputProps
+}) => {
 	const [inputType, setInputType] = useState('password')
 	const id = useId()
 
@@ -11,23 +47,32 @@ const PasswordInput = ({ className = '', label = 'Password', color = 'blue', ...
 		setInputType((prev) => prev === 'password' ? 'text' : 'password')
 	}
 
+	const labelClassName = classNames.overwriteLabel ?? `${defaultClassNames(variant)?.label} ${classNames.label ?? ''}`
+	const spanClassName = classNames.overwriteSpan ?? `${defaultClassNames(variant)?.span} ${classNames.span ?? ''}`
+	const inputClassName = classNames.overwriteInput ?? `${defaultClassNames(variant)?.input} ${classNames.input ?? ''}`
+	const divClassName = classNames.overwriteDiv ?? `${defaultClassNames(variant)?.div} ${classNames.div ?? ''}`
+	const buttonClassName = classNames.overwriteButton ?? `${defaultClassNames(variant)?.button} ${classNames.button ?? ''}`
+
 	return (
-		<label className='flex flex-row items-center gap-2 w-96' htmlFor={id}>
-			<span className='grow-0 shrink-0 min-w-[8ch]'>{label}</span>
-			<div className='grow shrink basis-full flex flex-row gap-2 ml-auto'>
+		<label className={labelClassName} htmlFor={id}>
+			<span className={spanClassName}>
+				{label}
+			</span>
+			<div className={divClassName}>
 				<input 
-				{...inputProps}
-				className={`${className} flex-none basis-full rounded bg-${color}-900 dark:bg-${color}-200 text-${color}-200 dark:text-${color}-900 p-1`}
-				type={inputType}
-				id={id}
+					className={inputClassName}
+					{...inputProps}
+					type={inputType}
+					id={id}
 				/> 
 				<button 
-				className={`grow-0 shrink-0 basis-8 grid place-items-center text-${color}-900 dark:text-${color}-200 fill-current`}
-				type='button'
-				onClick={switchInputType}>
+					className={buttonClassName}
+					type='button'
+					onClick={switchInputType}
+				>
 					{inputType === 'password' ? <OpenEye /> : <ClosedEye />}
 				</button>
-		</div>
+			</div>
 		</label>
 	)
 }
