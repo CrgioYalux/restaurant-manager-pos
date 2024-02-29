9716e76463faa7761edb6d67954880779a43ec5a
Sergio Yalux <randaledomman1@gmail.com>
2024-02-28 22:33:03 -0300

    feat: mkdir utils & create getCurrentDateTimeToJSON function

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,23 @@
+function getCurrentDateTimeToJSON() {
+    const current = new Date(Date.now())
+    const year = current.getFullYear()
+    const month = current.getMonth() + 1
+    const day = current.getDate()
+    const hours = current.getHours()
+    const minutes = current.getMinutes()
+    const seconds = current.getSeconds()
+
+    const formattedMonth = month < 10 ? `0${month}` : `${day}`
+    const formattedDay = day < 10 ? `0${day}` : `${day}`
+    const formattedHours = hours < 10 ? `0${hours}` : `${hours}`
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`
+
+    const result = `${year}-${formattedMonth}-${formattedDay}T${formattedHours}:${formattedMinutes}:${formattedSeconds}.000Z`
+
+    return result
+}
+
+export {
+    getCurrentDateTimeToJSON,
+}
