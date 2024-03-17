3ef0e277bd8d041ac6448895fc4a6906c01f5ab8
Sergio Yalux <randaledomman1@gmail.com>
2024-03-17 02:20:05 -0300

    feat: update example use case for interaction components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,78 +1,88 @@
-import DatePicker from "./input_components/DatePicker"
-import DateTimePicker from "./input_components/DateTimePicker"
-import PasswordInput from "./input_components/PasswordInput"
-import TextInput from "./input_components/TextInput"
-import TimePicker from "./input_components/TimePicker"
-import Textarea from "./input_components/Textarea"
-import CurrencyInput from "./input_components/CurrencyInput"
-import { useTheme } from "./providers/Theme"
+import Button from './components/Button'
+import Switcher from './components/Switcher'
+import SwitchThemeButton from './components/SwitchThemeButton'
+import { useTheme } from './providers/Theme'
 import { useState } from 'react'
 
+const options = ['initial', 'success', 'error', 'warning']
+
 const App = () => {
-    const [state, switchTheme] = useTheme()
-    const [dateTime, setDatetime] = useState()
-    const [date, setDate] = useState()
-    const [time, setTime] = useState()
-    const [text, setText] = useState()
-    const [textarea, setTextarea] = useState()
-    const [dollars, setDollars] = useState()
+    const [theme, switchTheme] = useTheme()
+    const [option, setOption] = useState(options[0])
 
     return (
-        <div className='min-h-screen dark:bg-gray-900 flex flex-col items-center justify-center'>
-            <h1 className='text-4xl font-bold text-blue-500'>Hello there</h1>
-            <h2 className='text-2xl font-bold text-blue-500'>Under construction</h2>
-
-            <div className='flex flex-col gap-y-4 items-start'>
-                <h3 className='text-xl text-black dark:text-white'>An example form</h3>
-                <PasswordInput label='Enter your password:' />
-                <DatePicker 
-                date={date}
-                setDate={setDate}
-                label='Choose your birthdate:'
-                variant='warning'
-                classNames={{
-                    overwriteLabel: 'flex gap-10 items-center border-2 p-2 rounded-xl cursor-pointer' 
-                }} />
-                <DateTimePicker label="Some datetime:" setDateTime={setDatetime} dateTime={dateTime} setToCurrentButton={true} />
-                <button onClick={switchTheme} className='text-black dark:text-white'>Go {state.opposite}</button>
-                <TimePicker time={time} setTime={setTime} includeSeconds />
-                <TimePicker variant='error' time={time} setTime={setTime} />
-                <TimePicker variant='success' label='some time' />       
-                <TextInput 
-                text={text}
-                setText={setText}
-                onlyAllow={/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm}
-                placeholder='e.g. john_lock30'
-                label={'An instagram-like username:'}
-                />
-                <TextInput 
-                text={text}
-                setText={setText}
-                allowSymbols={false}
-                allowLetters={false}
-                allowNumbers={false}
-                allowSpaces
-                allow={'-.'}
-                placeholder='e.g. ... --- ...'
-                variant='warning'
-                label='Some morse code:'
-                />
-                <Textarea 
-                label='Express a feeling:'
-                text={textarea} 
-                setText={setTextarea}
-                onlyLetters
-                allowSpaces
-                classNames={{ input: 'transition' }}
-                variant={
-                    textarea?.length === 0
-                    ? 'error'
-                    : textarea?.length === 10 
-                      ? 'success' : 'warning'
-                }
-                maxLength={10}
-                />
-                <CurrencyInput text={dollars} setText={setDollars} />
+        <div className='min-h-screen dark:bg-gray-900 flex flex-col items-center justify-center gap-4 dark:text-gray-100'>
+            <SwitchThemeButton />
+            <Switcher
+            label='Pick a variant:'
+            variant={option}
+            checked={option}
+            setChecked={setOption}
+            options={options}
+            switchOnAnyClick
+            switchDirection='left'
+            />
+            <Switcher
+            label='Pick a variant:'
+            variant={option}
+            checked={option}
+            setChecked={setOption}
+            options={options}
+            switchOnAnyClick={false}
+            />
+            <div className='flex flex-col gap-2 items-center'>
+                <h3 className=''>Initial Buttons</h3>
+                <div className='flex gap-2'>
+                    <Button 
+                    filled 
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                    <Button 
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                </div>
+            </div>
+            <div className='flex flex-col gap-2 items-center'>
+                <h3 className=''>Initial Buttons</h3>
+                <div className='flex gap-2'>
+                    <Button
+                    variant='success'
+                    filled 
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                    <Button 
+                    variant='success'
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                </div>
+            </div>
+            <div className='flex flex-col gap-2 items-center'>
+                <h3 className=''>Initial Buttons</h3>
+                <div className='flex gap-2'>
+                    <Button
+                    variant='error'
+                    filled 
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                    <Button 
+                    variant='error'
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                </div>
+            </div>
+            <div className='flex flex-col gap-2 items-center'>
+                <h3 className=''>Initial Buttons</h3>
+                <div className='flex gap-2'>
+                    <Button
+                    variant='warning'
+                    filled 
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                    <Button 
+                    variant='warning'
+                    onClick={switchTheme}
+                    >Switch theme</Button>
+                </div>
             </div>
         </div>
     )
